4c410373b116996d5a73d9ee7ab1c8b8b6d100cd
Ilya Dzeraziak <ilya.by@bk.ru>
2018-03-13 21:06:02 +0300

    Created .gitignore

--- NET.W.2018.Dzeraziak.01/Test/IsSorted.cs
+++ NET.W.2018.Dzeraziak.01/Test/IsSorted.cs
@@ -17,5 +17,17 @@ namespace Test
 
             Assert.Equal(arrGoal, arrInput);
         }
+
+        ///<summary>MergeSort Unity Test</summary>  
+        [Fact]
+        public void IsMergeSorted()
+        {
+            var arrGoal = new int[]{1, 2, 3, 4, 5, 6, 7};
+            var arrInput = new int[]{2, 1, 3, 5, 6, 7, 4};
+
+            Sorts.MergeSort(arrInput, 0, arrInput.Length - 1);
+
+            Assert.Equal(arrGoal, arrInput);
+        }
     }
 }
