fbe81be6c0d17a28678870cb16e4a926ae772bb8
Ilya Dzeraziak <ilya.by@bk.ru>
2018-03-13 21:28:57 +0300

    Code refactoring

--- NET.W.2018.Dzeraziak.01/Solution/Sorts.cs
+++ NET.W.2018.Dzeraziak.01/Solution/Sorts.cs
@@ -50,8 +50,8 @@ namespace Solution.Sorts
 
         #endregion
 
-        ///<summary>Custom merge sort implementation</summary>
-        public static void MergeSort(int[] input, int low, int high)
+    ///<summary>Custom merge sort implementation</summary>
+    public static void MergeSort(int[] input, int low, int high)
     {
         if (low < high)
         {
@@ -115,25 +115,6 @@ namespace Solution.Sorts
             input[low + i] = tmp[i];
         }
 
-    }
-
-    public static string PrintArray(int[] input)
-    {
-        string result = String.Empty;
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            result = result + input[i] + " ";
-        }
-        if (input.Length == 0)
-        {
-            result = "Array is empty.";
-            return result;
-        }
-        else
-        {
-            return result;
-        }
     }
     }    
 }
