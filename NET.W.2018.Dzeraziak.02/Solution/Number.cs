c27e42d9861bfe94e5ec3e19134ed89bf9ae8159
Ilya <ilya.by@bk.ru>
2018-03-15 14:19:02 +0300

    Created the template

--- NET.W.2018.Dzeraziak.02/Solution/Number.cs
+++ NET.W.2018.Dzeraziak.02/Solution/Number.cs
@@ -2,8 +2,21 @@
 
 namespace Solution.Number
 {
-    public class Number
+    public static class Number
     {
-        
+        public static int FindNextBiggerNumber(int Number)
+        {
+
+        }
+
+        public static int[] FilterDigit(int[] Digits)
+        {
+            return Digits;                        
+        }
+
+        public static int FindNthRoot(int number)
+        {
+            return number;
+        }
     }
 }
