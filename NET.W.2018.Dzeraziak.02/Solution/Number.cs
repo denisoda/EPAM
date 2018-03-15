6f40c437043aab92cf7a3f50abef3f2ec8f89b3f
Ilya <ilya.by@bk.ru>
2018-03-15 14:40:50 +0300

    edited summary

--- NET.W.2018.Dzeraziak.02/Solution/Number.cs
+++ NET.W.2018.Dzeraziak.02/Solution/Number.cs
@@ -11,16 +11,17 @@ namespace Solution.Number
         /// Finds the bigger closest number from the input
         /// </summary>
         /// <param name="Number"></param>
-        /// <returns></returns>
+        /// <returns>Filtered an int</returns>
         public static int FindNextBiggerNumber(int Number)
         {
 
         }
+
         /// <summary>
-        /// Filters input and return an array of num which consists this number
+        /// Filters input and  which consists this number
         /// </summary>
         /// <param name="Digits"></param>
-        /// <returns></returns>
+        /// <returns>an array of filtered nums</returns>
         public static int[] FilterDigit(int[] Digits)
         {
             return Digits;                        
@@ -29,7 +30,7 @@ namespace Solution.Number
         /// Finds a power of inputed number wiht Newton's method
         /// </summary>
         /// <param name="number"></param>
-        /// <returns></returns>
+        /// <returns>powered an int</returns>
         public static int FindNthRoot(int number)
         {
             return number;
