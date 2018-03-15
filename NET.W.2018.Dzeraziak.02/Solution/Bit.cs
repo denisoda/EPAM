9c3293baa320a14cd0c9a1f29c419a8151fb02c5
Ilya <ilya.by@bk.ru>
2018-03-15 14:26:21 +0300

    Added the summary to the  Bit class

--- NET.W.2018.Dzeraziak.02/Solution/Bit.cs
+++ NET.W.2018.Dzeraziak.02/Solution/Bit.cs
@@ -4,6 +4,14 @@ namespace Solution.Bit
 {
     public static class Bit
     {
+        /// <summary>
+        /// bits of the second number took positions from bit j
+        /// by bits i (bits are numbered from right to left).
+        /// </summary>
+        /// <param name="numberSource"></param>
+        /// <param name="numberln"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
         public static void InsertNumber(int numberSource, int numberln, int i, int j)
         {
 
