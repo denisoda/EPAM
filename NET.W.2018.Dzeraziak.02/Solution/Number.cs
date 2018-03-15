55bdbacc4b5284d9b8b63ed600114de578b1727e
Ilya <ilya.by@bk.ru>
2018-03-15 14:24:10 +0300

    Added a summary

--- NET.W.2018.Dzeraziak.02/Solution/Number.cs
+++ NET.W.2018.Dzeraziak.02/Solution/Number.cs
@@ -2,18 +2,34 @@
 
 namespace Solution.Number
 {
+    /// <summary>
+    /// Number manimpulation class
+    /// </summary>
     public static class Number
     {
+        /// <summary>
+        /// Finds the bigger closest number from the input
+        /// </summary>
+        /// <param name="Number"></param>
+        /// <returns></returns>
         public static int FindNextBiggerNumber(int Number)
         {
 
         }
-
+        /// <summary>
+        /// Filters input and return an array of num which consists this number
+        /// </summary>
+        /// <param name="Digits"></param>
+        /// <returns></returns>
         public static int[] FilterDigit(int[] Digits)
         {
             return Digits;                        
         }
-
+        /// <summary>
+        /// Finds a power of inputed number wiht Newton's method
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
         public static int FindNthRoot(int number)
         {
             return number;
