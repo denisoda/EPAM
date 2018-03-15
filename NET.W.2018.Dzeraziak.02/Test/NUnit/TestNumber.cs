78760b8888e3460e50a90e28f87ab74a37218574
Ilya <ilya.by@bk.ru>
2018-03-15 15:01:36 +0300

    Added the NUnit test

--- NET.W.2018.Dzeraziak.02/Test/NUnit/TestNumber.cs
+++ NET.W.2018.Dzeraziak.02/Test/NUnit/TestNumber.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using Solution.Number;
 
 namespace Solution.Number.NUnit
 {
     public class TestNumber
     {
         [Fact]
-        public void Test()
+        [TestCase(12, ExpectedResult = 21)]
+        [TestCase(513, ExpectedResult = 531)]
+        [TestCase(2017, ExpectedResult = 2071)]
+        [TestCase(414, ExpectedResult = 441)]
+        [TestCase(144, ExpectedResult = 414)]
+        [TestCase(1234321, ExpectedResult = 1241233)]
+        [TestCase(1234126, ExpectedResult = 1234162)]
+        [TestCase(3456432, ExpectedResult = 3462345)]
+        [TestCase(10, ExpectedResult = -1)]
+        [TestCase(20, ExpectedResult = -1)]
+        public void IsReturnCorrect(int value)
         {
+            Number.FindNextBiggerNumber(value);
 
+            Assert.Equal(value, ExpectedResult);
         }
     }
 }
