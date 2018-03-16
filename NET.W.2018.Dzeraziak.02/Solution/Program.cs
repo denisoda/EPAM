ec6e2561062a5688d08693f325b99728a37e5a30
Ilya <ilya.by@bk.ru>
2018-03-16 23:11:53 +0300

    Added a Solution template and Readme

--- NET.W.2018.Dzeraziak.02/Solution/Program.cs
+++ NET.W.2018.Dzeraziak.02/Solution/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Solution.Number;
 
 namespace Solution
 {
@@ -6,7 +7,7 @@ namespace Solution
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");
+            System.Console.WriteLine(Number.FindNextBiggerNumber(12));
         }
     }
 }
