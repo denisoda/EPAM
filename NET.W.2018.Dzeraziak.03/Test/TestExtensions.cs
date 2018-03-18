48aa422922e308a568d428037f34740a01ab588c
Ilya Dzeraziak <ilya.by@bk.ru>
2018-03-18 17:59:09 +0300

    Refactoring and documenting

--- NET.W.2018.Dzeraziak.03/Test/TestExtensions.cs
+++ NET.W.2018.Dzeraziak.03/Test/TestExtensions.cs
@@ -1,13 +1,24 @@
 using NUnit.Framework;
 using Solution.Extensions;
 
-namespace Solution.Tests
+namespace Solution.Extensions.Tests
 {
+    /// <summary>
+    /// Class for testing extensions methods
+    /// </summary>
     public class TestExtensions
     {
         [Test]
-        [TestCase(5, ExpectedResult = "101")]
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000001")]
+        [TestCase(double.NaN, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.NegativeInfinity, ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.PositiveInfinity, ExpectedResult = "0111111111110000000000000000000000000000000000000000000000000000")]
         public string IsCorrectConvertationDoubleToBinaryInStringFormat(double number) 
-        => Extensions.Extensions.GetBinaryString(number);
+        => Extensions.GetBinaryString(number);
     }
 }
