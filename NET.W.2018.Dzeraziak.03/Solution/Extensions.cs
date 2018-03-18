b70d7fbcc4519196ce2e6368c79609bf7637f1ae
Ilya Dzeraziak <ilya.by@bk.ru>
2018-03-18 18:03:26 +0300

    Commenting

--- NET.W.2018.Dzeraziak.03/Solution/Extensions.cs
+++ NET.W.2018.Dzeraziak.03/Solution/Extensions.cs
@@ -4,6 +4,11 @@ namespace Solution.Extensions
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Converts to the binary string
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>Binary representation of the number</returns>
         public static string GetBinaryString(this double number){ 
             var a = Convert.ToString(BitConverter.DoubleToInt64Bits(number), 2);
             return BitConverter.GetBytes(number).ToString();
