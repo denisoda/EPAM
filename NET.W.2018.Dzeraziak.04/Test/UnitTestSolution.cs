3a6cafc9b4cc0059896fb6ae552bfe9bb4a69d13
Ilya <ilya.by@bk.ru>
2018-03-21 19:21:36 +0300

    Createad a solution

---
+++ NET.W.2018.Dzeraziak.04/Test/UnitTestSolution.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Solution.Tests.NUnit
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
