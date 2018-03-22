1a841668fe739311fba6b221bed36b92389ee9a1
Ilya Dzeraziak <ilya.by@bk.ru>
2018-03-22 17:46:28 +0300

    set-up the project

--- NET.W.2018.Dzeraziak.04/Solution/Polynomial.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Solution
-{
-    public class Polynomial
-    {
-        
-    }
-}
---
+++ NET.W.2018.Dzeraziak.05/Solution/Polynomial.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Solution
+{
+    public class Polynomial
+    {
+        
+    }
+}
