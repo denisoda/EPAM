d61915f7643ebf88f8fa4d5a36bacfcb59aee4dc
Ilya Dzeraziak <ilya.by@bk.ru>
2018-03-28 20:30:01 +0300

    Set-uped the solutions

---
+++ NET.W.2018.Dzeraziak.08/SolutonBankAccount/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolutonBankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
