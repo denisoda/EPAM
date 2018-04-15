2a243e62ee334c6171ec04a633ba561a56b34258
Ilya <ilya.by@bk.ru>
2018-04-15 11:15:05 +0300

    Added the fibo generator

---
+++ NET.W.2018.Dzeraziak.08/Fibo/Fibo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Fibo
+{
+    public static class Fibo 
+    {
+        public static IEnumerable Generate(int number)
+        {
+            for(int i = 0; i < number; i++)
+            {
+                int first = 0;
+                int second = 1;
+                int temp = first + second;
+
+                first = second;
+                second = temp;
+
+                yield return first;
+            }
+        }
+    }
+}
