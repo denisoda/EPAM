1b6ec7d4af6704eec3fdc7644e5809a59351e4e7
Ilya Dzeraziak <ilya.by@bk.ru>
2018-04-17 22:56:35 +0300

    An implementation of a  binary serach

--- NET.W.2018.Dzeraziak.11-12/BinarySearch/BinarySearch.cs
+++ NET.W.2018.Dzeraziak.11-12/BinarySearch/BinarySearch.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace BinarySearch 
 {
@@ -11,10 +12,40 @@ namespace BinarySearch
         /// <param name="searchArr">array for search</param>
         /// <param name="element">element for search</param>
         /// <returns>Searched element</returns>
-        public T Search(ref T[] searchArr, T element)
+        public static void Search<T>(T[] array, T searchFor, Comparer<T> comparer)
         {
-            
-            return element;
+
+            int high, low, mid;
+            high = array.Length - 1;
+            low = 0;
+            if (array[0].Equals(searchFor))
+                Console.WriteLine("Value {0} Found At Index {1}", array[0], 0);
+            else if (array[high].Equals(searchFor))
+                Console.WriteLine("Value {0} Found At Index {1}", array[high], high);
+            else
+            {
+                while (low <= high)
+                {
+                    mid = (high + low) / 2;
+                    if (comparer.Compare(array[mid], searchFor) == 0)
+                    {
+                        Console.WriteLine("Value {0} Found At Index {1}", array[mid], mid);
+                        break;
+                    }
+                    else
+                    {
+                        if (comparer.Compare(searchFor, array[mid]) > 0)
+                            high = mid + 1;
+                        else
+                            low = mid + 1;
+                    }
+
+                }
+                if (low > high)
+                {
+                    Console.WriteLine("Value Not Found In the Collection");
+                }
+            }
         }
     }
 }
