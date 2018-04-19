b65f4bf330106def057c223e638e0a77c2ab295d
Ilya Dzeraziak <ilya.by@bk.ru>
2018-04-19 20:06:02 +0300

    Refactoring

--- NET.W.2018.Dzeraziak.11-12/Test/BinarySearch/BinarySearch_SortedArray_IsTrue.cs
+++ NET.W.2018.Dzeraziak.11-12/Test/BinarySearch/BinarySearch_SortedArray_IsTrue.cs
@@ -19,13 +19,3 @@ namespace Tests
         }
     }
 }
-
-//public class Tests
-//{
-//    [TestCase(new int[] { 15, 22, 49, 5645, 44, 0, 432, -456, 99 }, (int)5)]
-//    [Test]
-//    public void BinarySearch_SortedArray_HasFoundCorrectItem<T>(T[] array, T searchFor)
-//    {
-//        Assert.AreEqual(searchFor, BinarySearch<int>.Search(array, searchFor, System.Collections.Comparer.Default));
-//    }
-//}
