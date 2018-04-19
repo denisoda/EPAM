de733c6dab45def0f58064b59eb76938435f1f13
Ilya Dzeraziak <ilya.by@bk.ru>
2018-04-19 20:26:56 +0300

    Stopwatch refactoring

---
+++ NET.W.2018.Dzeraziak.11-12/Stopwatch/ElapsedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    internal class ElapsedEventArgs
+    {
+        public readonly TimeSpan Time;
+        public readonly string NotifMessage;
+
+        public ElapsedEventArgs(TimeSpan time, string notifMessage)
+        {
+            NotifMessage = notifMessage;
+            Time = time;
+        }
+    }
+}
---
+++ NET.W.2018.Dzeraziak.11-12/Stopwatch/Timer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Stopwatch
+{
+    class Timer
+    {
+        #region Private fields
+
+        private bool _isRunnig;
+
+        #endregion
+
+        #region Constructors
+        
+        /// <summary>
+        /// Constructor for initilazing
+        /// </summary>
+        Timer()
+        {
+        }
+
+        #endregion
+
+        #region Events
+
+        public EventHandler<ElapsedEventArgs> OnTimeElapsed;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// setting the time for the timer
+        /// </summary>
+        /// <param name="time"></param>
+        /// <param name="notifMessage"></param>
+        public void SetTime(TimeSpan time, string notifMessage)
+        {
+            Invoke(time);
+            OnTimeElapsed?.Invoke(this, new ElapsedEventArgs(time, notifMessage));
+        }
+
+        #endregion
+
+        #region Privat methods
+
+        public void Invoke(TimeSpan time)
+        {
+            _isRunnig = true;
+
+            int mileseconds = time.Milliseconds;
+            Thread.Sleep(mileseconds);
+
+            _isRunnig = false;
+        }
+
+        #endregion
+    }
+}
