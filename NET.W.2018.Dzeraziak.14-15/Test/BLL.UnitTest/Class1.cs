6effc46bea1b6afb04023abd1198bffa83ae5358
Ilya Dzeraziak <ilya.by@bk.ru>
2018-04-23 23:19:23 +0300

    Created the unit test

---
+++ NET.W.2018.Dzeraziak.14-15/Test/BLL.UnitTest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Ninject;
+
+namespace BLL.UnitTest
+{   
+    [TestFixture]
+    class Test
+    {
+        [Test]
+        public void test()
+        {
+        }
+    }
+}
